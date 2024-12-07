892ec404987193a1e715e1fc8f9f4f562f5c52b4
Faith Wanjiku <wanjikuf141@gmail.com>
2024-12-08 02:30:40 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  let left = parseInt(dodger.style.left.replace("px", ""), 10);
+  if (left > 0) {
+    dodger.style.left = (left - 1) + "px";
+  }
+}
+
+function moveDodgerRight() {
+  let left = parseInt(dodger.style.left.replace("px", ""), 10);
+  if (left < 360) {
+    dodger.style.left = (left + 1) + "px";
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
